67ff6c54341a026955b43f0f3abca31acb6a9b7e
unknown <siwady0908@unitec.edu>
2014-08-30 17:33:29 -0600

    UserProfile added

--- app/src/main/java/com/example/siwady/tevendo/UserProfile.java
+++ app/src/main/java/com/example/siwady/tevendo/UserProfile.java
@@ -5,72 +5,65 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ListView;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.siwady.tevendo.R;
 import com.parse.FindCallback;
 import com.parse.GetCallback;
-import com.parse.Parse;
 import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseImageView;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
 import java.util.List;
 
-public class UserPage extends Activity {
+public class UserProfile extends Activity {
 
-    String Email;
-    TextView MyArticles;
     TextView UserName;
+    TextView UserEmail;
+    TextView Nationality;
+    ParseImageView UserImage;
+    String Email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_page);
+        setContentView(R.layout.activity_user_profile);
+        UserName= (TextView) findViewById(R.id.tv_UserName);
+        UserEmail= (TextView) findViewById(R.id.tv_Email);
+        Nationality= (TextView) findViewById(R.id.tv_Nationality);
+        UserImage= (ParseImageView) findViewById(R.id.iv_UserImage);
         Intent i = getIntent();
         Email = i.getStringExtra ("Email");
-        MyArticles=(TextView)findViewById(R.id.tv_MyArticles);
-        UserName=(TextView) findViewById(R.id.tv_User);
-        UserName.setText(getUserName());
-        MyArticles.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                    Intent to_MyArticles = new Intent(UserPage.this,MyArticles.class);
-                    to_MyArticles.putExtra("Email",Email);
-                    startActivity(to_MyArticles);
-            }
-        });
-
-        UserName.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent to_UserProfile = new Intent(UserPage.this,UserProfile.class);
-                to_UserProfile.putExtra("Email",Email);
-                startActivity(to_UserProfile);
-            }
-        });
+        fill();
     }
 
-    private String getUserName() {
+    private void fill() {
         ParseQuery<ParseObject> query = ParseQuery.getQuery("User");
-        query.whereEqualTo("Email", Email);
+        query.whereEqualTo("Email",Email);
         query.getFirstInBackground(new GetCallback<ParseObject>() {
             @Override
             public void done(ParseObject parseObject, com.parse.ParseException e) {
+
                 if (parseObject != null) {
                     UserName.setText(parseObject.get("UserName").toString());
+                    UserEmail.setText(Email);
+                    //Nationality.setText(parseObject.get("Nationality").toString());
+
+                    UserImage.setParseFile(parseObject.getParseFile("UserProfileImage"));
+                    UserImage.loadInBackground();
                 }
             }
         });
-        return null;
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.user_page, menu);
+        getMenuInflater().inflate(R.menu.user_profile, menu);
         return true;
     }
 
